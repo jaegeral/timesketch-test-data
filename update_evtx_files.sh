81316fdde7d1a9411a6b0ea02940efde027be8a9
Alexander J <741037+jaegeral@users.noreply.github.com>
2022-09-20 13:48:49 +0200

    Update update_evtx_files.sh

--- update_evtx_files.sh
+++ update_evtx_files.sh
@@ -10,6 +10,6 @@ for file in $(find ../EVTX-ATTACK-SAMPLES/ -type f -name "*.evtx"); do
         dirname=$(dirname $relpath)
         echo "dirname $dirname"
         mkdir -p ./EVTX-ATTACK-SAMPLES-plaso/$dirname
-        sudo docker run -v /Users/jaegeral/scripts/:/data log2timeline/plaso log2timeline /data/timesketch-test-data/EVTX-ATTACK-SAMPLES-plaso/$relpath.plaso /data/$relpath
+        sudo docker run -v /Users/jaegeral/scripts/:/data log2timeline/plaso log2timeline --storage-file /data/timesketch-test-data/EVTX-ATTACK-SAMPLES-plaso/$relpath.plaso /data/$relpath
     fi
 done
